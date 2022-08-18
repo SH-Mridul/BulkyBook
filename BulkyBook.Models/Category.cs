d907d78de028f7ef246bb6c08a8c20af9c349c7b
mridulhossain <mridulcse8@gmail.com>
2022-08-18 11:51:15 +0600

    convert into n-tier architecture

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Name of Category"), Required(ErrorMessage = "Enter category name!")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order"), Range(1, int.MaxValue, ErrorMessage = "please check the value again!")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
